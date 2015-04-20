09af289a03320e9a0fca46fe93b1b931e32edac6
user <loma7958@colorado.edu>
2015-04-20 14:21:42 -0600

    added the basic idea of the rest of the ten public methods as well as the category struct

--- Database.h
+++ Database.h
@@ -5,6 +5,7 @@
 struct Item
 {
     std::string category;
+    std::string name;
     std::string description;
 
     Item()
@@ -12,14 +13,32 @@ struct Item
 
     }
 
-    Item(std::string in_title, int in_year)
+    Item(std::string inCategory, std::string inName, std::string inDescription)
     {
-        title = in_title;
-        year = in_year;
+        category = inCategory;
+        name = inName;
+        description = inDescription;
     }
 
 };
 
+struct Category
+{
+    std::string name;
+    std::string description;
+    std::vector<Item> itemVector;
+
+    Category()
+    {
+
+    }
+
+    Category(std::string inName, std::string inDescription)
+    {
+        name = inName;
+        description = inDescription;
+    }
+};
 class Database
 {
     public:
@@ -27,10 +46,35 @@ class Database
         ~Database();
 
         //inserts an item into the database
-        void insertItem(std::string category, std::string description);
+        void insertItem(std::string category, std::string name, std::string description);
 
         //prints the information of all the items in the database
         void printDatabase();
+
+        //prints the information of all the items within a category
+        void printCategory(std::string category);
+
+        //finds an item and prints all of the information related to it
+        void printItem(std::string category, std:string name);
+
+        //prints all of the categories within the database
+        void printAllCategories();
+
+        //changes the category of an item
+        void changeItemCategory(std::string oldCategory, std::string newCategory, string name);
+
+        //changes the name of an item
+        void changeItemName(std::string category, std::string oldName, std:string newName);
+
+        //changes the description of an item
+        void changeItemDescription(std::string category, std::string name, std::newDescription);
+
+        //deletes a category
+        void deleteCategory(std::string category);
+
+        //deletes an item
+        void deleteItem(std::category, std::name);
+
 
     protected:
     private:
