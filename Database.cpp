e6dd9d6279c9849619997e6dee275b9798829a9e
user <loma7958@colorado.edu>
2015-04-20 18:45:20 -0600

    added theframework code (a few comments, the method prototypes) for te Database.cpp file

---
+++ Database.cpp
@@ -0,0 +1,79 @@
+#include "Database.h"
+#include<vector>
+#include<iostream>
+#include <algorithm>
+Database::Database(int size)
+{
+
+}
+
+Database::~Database()
+{
+
+}
+
+//inserts an item into the database
+void Database::insertItem(std::string category, std::string name, std::string description)
+{
+
+}
+
+//prints the information of all the items in the database
+void Database::printDatabase()
+{
+
+}
+
+//prints the information of all the items within a category
+void Database::printCategory(std::string category)
+{
+
+}
+
+//finds an item and prints all of the information related to it
+void Database::printItem(std::string category, std::string name)
+{
+
+}
+
+//prints all of the categories within the database
+void Database::printAllCategories()
+{
+
+}
+
+//changes the category of an item
+void Database::changeItemCategory(std::string oldCategory, std::string newCategory, std::string name)
+{
+
+}
+
+//changes the name of an item
+void Database::changeItemName(std::string category, std::string oldName, std::string newName)
+{
+
+}
+
+//changes the description of an item
+void Database::changeItemDescription(std::string category, std::string name, std::string newDescription)
+{
+
+}
+
+//deletes a category
+void Database::deleteCategory(std::string category)
+{
+
+}
+
+//deletes an item
+void Database::deleteItem(std::string category, std::string name)
+{
+
+}
+
+//calculates a hashSum
+int Database::hashSum(std::string stringToHash, int tableSize)
+{
+
+}
