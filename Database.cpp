f7af6f3df18b2a840a9754fbd2c05cb20bb7e5bb
user <loma7958@colorado.edu>
2015-04-21 10:58:25 -0600

    finished all of the methods

--- Database.cpp
+++ Database.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 Database::Database(int size)
 {
+    tableSize = size;
     for(int i = 0; i < size; i++)
     {
         std::vector<Category> categoryVector;
@@ -16,67 +17,218 @@ Database::~Database()
 
 }
 
+//inserts a category into the database
+void Database::insertCategory(std::string name, std::string description)
+{
+    Category newCategory(name, description);
+    int sum = hashSum(name);
+    tableVector[sum].push_back(newCategory);
+    numOfCategories++;
+}
+
 //inserts an item into the database
 void Database::insertItem(std::string category, std::string name, std::string description)
 {
-
+    Item newItem(category, name, description);
+    int sum = hashSum(category);
+    for(int z = 0; z<tableVector[sum].size(); z++)
+    {
+        if(tableVector[sum][z].name == category)
+        {
+            tableVector[sum][z].itemVector.push_back(newItem);
+        }
+    }
+    numOfItems ++;
 }
 
-//prints the information of all the items in the database
+//prints the information of all the items and categories in the database
 void Database::printDatabase()
 {
-
+    if(numOfCategories == 0)
+    {
+        std::cout<<"empty"<<std::endl;
+    }
+    for(int z = 0; z< tableVector.size(); z++)
+    {
+        for(int i = 0; i < tableVector[z].size(); i++)
+        {
+            std::cout<<tableVector[z][i].name<<" : "<<tableVector[z][i].description<<std::endl;
+            for(int y = 0; y < tableVector[z][i].itemVector.size(); y++)
+            {
+                std::cout<<"--"<<tableVector[z][i].itemVector[y].name<<" : "<<tableVector[z][i].itemVector[y].description<<std::endl;
+            }
+        }
+    }
 }
 
 //prints the information of all the items within a category
 void Database::printCategory(std::string category)
 {
-
+    int sum = hashSum(category);
+    for(int i = 0; i < tableVector[sum].size(); i++)
+    {
+        if(tableVector[sum][i].name == category)
+        {
+            std::cout<<tableVector[sum][i].name<<" : "<<tableVector[sum][i].description<<std::endl;
+            for(int y = 0; y < tableVector[sum][i].itemVector.size(); y++)
+            {
+                std::cout<<"--"<<tableVector[sum][i].itemVector[y].name<<" : "<<tableVector[sum][i].itemVector[y].description<<std::endl;
+            }
+        }
+    }
 }
 
 //finds an item and prints all of the information related to it
 void Database::printItem(std::string category, std::string name)
 {
-
+    int sum = hashSum(category);
+    for(int i = 0; i < tableVector[sum].size(); i++)
+    {
+        if(tableVector[sum][i].name == category)
+        {
+            for(int y = 0; y < tableVector[sum][i].itemVector.size(); y++)
+            {
+                if(tableVector[sum][i].itemVector[y].name == name)
+                {
+                    std::cout<<tableVector[sum][i].itemVector[y].name<<" : "<<tableVector[sum][i].itemVector[y].description<<std::endl;
+                }
+            }
+        }
+    }
 }
 
 //prints all of the categories within the database
 void Database::printAllCategories()
 {
-
+    if(numOfCategories == 0)
+    {
+        std::cout<<"empty"<<std::endl;
+    }
+    for(int z = 0; z< tableVector.size(); z++)
+    {
+        for(int i = 0; i < tableVector[z].size(); i++)
+        {
+            std::cout<<tableVector[z][i].name<<" : "<<tableVector[z][i].description<<std::endl;
+        }
+    }
 }
 
 //changes the category of an item
 void Database::changeItemCategory(std::string oldCategory, std::string newCategory, std::string name)
 {
+    std::string description;
+    int sum = hashSum(oldCategory);
+    for(int i = 0; i < tableVector[sum].size(); i++)
+    {
+        if(tableVector[sum][i].name == oldCategory)
+        {
+            for(int y = 0; y < tableVector[sum][i].itemVector.size(); y++)
+            {
+                if(tableVector[sum][i].itemVector[y].name == name)
+                {
+                    description = tableVector[sum][i].itemVector[y].description;
+                    tableVector[sum][i].itemVector.erase(tableVector[sum][i].itemVector.begin()+y);
+                }
+            }
+        }
+    }
+    Item itemToMove(newCategory, name, description);
+    sum = hashSum(newCategory);
+    for(int i = 0; i < tableVector[sum].size(); i++)
+    {
+        if(tableVector[sum][i].name == newCategory)
+        {
+            tableVector[sum][i].itemVector.push_back(itemToMove);
+        }
+    }
 
 }
 
 //changes the name of an item
 void Database::changeItemName(std::string category, std::string oldName, std::string newName)
 {
+    int sum = hashSum(category);
+    for(int i = 0; i < tableVector[sum].size(); i++)
+    {
+        if(tableVector[sum][i].name == category)
+        {
+            for(int y = 0; y < tableVector[sum][i].itemVector.size(); y++)
+            {
+                if(tableVector[sum][i].itemVector[y].name == oldName)
+                {
+                    tableVector[sum][i].itemVector[y].name = newName;
+                }
+            }
+        }
+    }
 
 }
 
 //changes the description of an item
 void Database::changeItemDescription(std::string category, std::string name, std::string newDescription)
 {
-
+    int sum = hashSum(category);
+    for(int i = 0; i < tableVector[sum].size(); i++)
+    {
+        if(tableVector[sum][i].name == category)
+        {
+            for(int y = 0; y < tableVector[sum][i].itemVector.size(); y++)
+            {
+                if(tableVector[sum][i].itemVector[y].name == name)
+                {
+                    tableVector[sum][i].itemVector[y].description = newDescription;
+                }
+            }
+        }
+    }
 }
 
 //deletes a category
 void Database::deleteCategory(std::string category)
 {
-
+    int sum = hashSum(category);
+    for(int i = 0; i < tableVector[sum].size(); i++)
+    {
+        if(tableVector[sum][i].name == category)
+        {
+            tableVector[sum].erase(tableVector[sum].begin()+i);
+        }
+    }
+    numOfCategories--;
 }
 
 //deletes an item
 void Database::deleteItem(std::string category, std::string name)
 {
-
+    int sum = hashSum(category);
+    for(int i = 0; i < tableVector[sum].size(); i++)
+    {
+        if(tableVector[sum][i].name == category)
+        {
+            for(int y = 0; y < tableVector[sum][i].itemVector.size(); y++)
+            {
+                if(tableVector[sum][i].itemVector[y].name == name)
+                {
+                    tableVector[sum][i].itemVector.erase(tableVector[sum][i].itemVector.begin()+y);
+                }
+            }
+        }
+    }
+    numOfItems--;
 }
+/*
+    int hashSum(string)
 
-//calculates a hashSum
+    Calculates a hashsum based on the string passed in as well as the table size that the Database was created with.
+    All of the decimal ascii values of each character of the string are added together, then modded by the table size.
+    The resulting number is the hashsum.
+
+    hashSum(Dog) with a table size of 10 will return 2
+
+    pre-conditions: none
+
+    post-conditions: none
+*/
 int Database::hashSum(std::string stringToHash)
 {
     int sum = 0;
--- Database.h
+++ Database.h
@@ -42,8 +42,11 @@ struct Category
 class Database
 {
     public:
-        Database(int);
+        Database(int size);
         ~Database();
+
+        //inserts a category into the database
+        void insertCategory(std::string name, std::string description);
 
         //inserts an item into the database
         void insertItem(std::string category, std::string name, std::string description);
@@ -55,31 +58,31 @@ class Database
         void printCategory(std::string category);
 
         //finds an item and prints all of the information related to it
-        void printItem(std::string category, std:string name);
+        void printItem(std::string category, std::string name);
 
         //prints all of the categories within the database
         void printAllCategories();
 
         //changes the category of an item
-        void changeItemCategory(std::string oldCategory, std::string newCategory, string name);
+        void changeItemCategory(std::string oldCategory, std::string newCategory, std::string name);
 
         //changes the name of an item
-        void changeItemName(std::string category, std::string oldName, std:string newName);
+        void changeItemName(std::string category, std::string oldName, std::string newName);
 
         //changes the description of an item
-        void changeItemDescription(std::string category, std::string name, std::newDescription);
+        void changeItemDescription(std::string category, std::string name, std::string newDescription);
 
         //deletes a category
         void deleteCategory(std::string category);
 
         //deletes an item
-        void deleteItem(std::category, std::name);
+        void deleteItem(std::string category, std::string name);
 
 
     protected:
     private:
-        //calculates a hashSum using x, which is the string to hash, as well as s, which is the array size of the table
-        int hashSum(std::string x, int s);
+        //calculates a hashSum
+        int hashSum(std::string stringToHash);
 
         //number of slots in the hash table
         int tableSize = 0;
@@ -87,8 +90,11 @@ class Database
         //keeps track of the number of items in the hash table
         int numOfItems = 0;
 
+        //keeps track of the number of categories in the hash table
+        int numOfCategories = 0;
+
         //table vector
-        std::vector<std::vector<Item> > tableVector;
+        std::vector<std::vector<Category> > tableVector;
 };
 
 #endif // DATABASE_H
